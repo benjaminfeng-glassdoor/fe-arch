82c13af50b57fc0ca91d4c7e38c4ec1401a23d7a
Benjamin Feng <benjamin.feng@glassdoor.com>
2020-02-28 14:14:50 -0600

    Dynamic posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
 import Layout from "../components/Layout";
+import Link from "next/link";
+
+function PostLink(props: { id: string }) {
+  return (
+    <li>
+      <Link href="/post/[id]" as={`/post/${props.id}`}>
+        <a>{props.id}</a>
+      </Link>
+    </li>
+  );
+}
 
 export default function Index() {
   return (
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink id="hello-nextjs" />
+        <PostLink id="learn-nextjs" />
+        <PostLink id="deploy-nextjs" />
+      </ul>
     </Layout>
   );
 }
